275f81814af7845cd6e09a0e95b14e7b52a4bdbd
unamed666 <52182376+eroge69@users.noreply.github.com>
2025-03-20 00:41:47 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -497,7 +497,7 @@ export default function PyToExeConverter() {
       </p>
 
       <p className="mt-8 text-center text-sm text-muted-foreground">
-        The file is uploaded to{" "}
+        The file will be uploaded to{" "}
         <a href="https://github.com/eroge69/PyToExe/tree/main/python-files" target="_blank" rel="noopener noreferrer">
           <b> THIS REPOSITORY</b>
         </a>
